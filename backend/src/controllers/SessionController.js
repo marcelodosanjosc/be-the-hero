99e76ca929bba530a37a34cf46256ddc1cbc32d7
marcelodosanjosc <marcelodosanjosc@gmail.com>
2020-07-13 20:44:27 -0400

    Front-End criado

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -2,10 +2,10 @@ const connectino = require('../database/connection')
 
 module.exports = {
     async create(request, response) {
-        const {id } = request.body
+        const { id } = request.body
 
         const ong = await connectino('ongs')
-        .where('id',id).select('name').first()
+        .where('id', id).select('name').first()
         
         if (!ong){
             return response.status(400).json({error: 'Não exite ID para essa ONG'})
